9bd36012f0f77a62b3d1856daa618ee393388fde
ravidora2004 <42553644+ravidora2004@users.noreply.github.com>
2025-04-08 09:35:26 -0500

    Update qtc-header.component.ts

--- src/app/qtc-header/qtc-header.component.ts
+++ src/app/qtc-header/qtc-header.component.ts
@@ -1,31 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../services/common.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-qtc-header',
   templateUrl: './qtc-header.component.html',
-  styleUrl: './qtc-header.component.css'
+  styleUrl: './qtc-header.component.scss'
 })
-export class QtcHeaderComponent {
-modalOpen: boolean = false;
-  urlToOpen: any= '';
+export class QtcHeaderComponent implements OnInit {
 
-  constructor(private domSanitizer: DomSanitizer) {
-   this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        const url = event.urlAfterRedirects;
+  userName: string = '';
+  claimantId: number = 0;
+  caseId: number = 1;
+  notesRequired: boolean = false;
+  urlToOpen: any = '';
+  externalUrl: string = ''
+  appointmentsIconRequired: boolean = false;
+  claimantLevel:string = 'CASE LEVEL';
 
-        // Example logic for determining claimantLevel
-        if (url.includes('/admin')) {
-          this.claimantLevel = 'Admin';
-        } else if (url.includes('/provider')) {
-          this.claimantLevel = 'Provider';
-        } else if (url.includes('/user')) {
-          this.claimantLevel = 'User';
-        } else {
-          this.claimantLevel = 'Guest';
-        }
-      });
+  constructor(private readonly commonService: CommonService,
+    private readonly router: Router,
+  ) {   
+    debugger;
+      
+  
+}
+
+  
+
+  ngOnInit() {
+    this.commonService.userName$.subscribe(userName => {
+      this.userName = userName;
+    });
+
+    this.commonService.currentFlag.subscribe(flag => {
+      this.notesRequired = flag;
+    });
+
+    this.commonService.currentAppointmentsFlag.subscribe(appointmentsflag => {
+      debugger;
+      this.appointmentsIconRequired = appointmentsflag;
+     
+    });
+
+    this.commonService.claimantIdSubject$.subscribe(claimantId => {
+      this.claimantId = claimantId;
+    });
+
+    this.commonService.caseIdSubject$.subscribe(caseId => {
+      this.caseId = caseId;
+    });
+
+    this.externalUrl = this.commonService.getReferrerUrl();
   }
+
+  openNotesWindow() {
+    let notesUrl = this.externalUrl + 'notes/Q_NOTES_01.asp?claimant_Id=' + this.claimantId + '&case_Id=' + this.caseId;
+    window.open(notesUrl, 'Notes', 'toolbar=no, scrollbars=yes, resizable=yes, top=100, left=100, width=900, height=800');
   }
+
+  openAppointmentsWindow() {
+    debugger;
+    const url = this.router.serializeUrl(this.router.createUrlTree(['withAppointment'], {
+      queryParams: {
+        claimantId: this.claimantId,
+        caseid: this.caseId
+      }
+    }));
+    
+     
+    
+    const fullUrl = `${window.location.origin}${url}`;
+    console.log(fullUrl);
+    window.open(fullUrl, '_blank');  
+
+  }
+
 }
