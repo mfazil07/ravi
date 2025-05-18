cafad191d650622dcc77bb8ba60d06fd8a059208
ravidora2004 <42553644+ravidora2004@users.noreply.github.com>
2025-05-18 12:08:43 -0500

    Update search.component.ts

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,3 +1,8 @@
 <script src="/Scripts/QTC_Scripts/PositiveContact/bootstrap.min.js"></script>
 
   * Bootstrap v3.3.6 (http://getbootstrap.com)
+
+
+                      <script src="/Scripts/QTC_Scripts/PositiveContact/jquery.min.js"></script>
+
+                      /*! jQuery v1.11.3 | (c) 2005, 2015 jQuery Foundation, Inc. | jquery.org/license */
