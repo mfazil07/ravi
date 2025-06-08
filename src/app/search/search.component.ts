b7ba24fbc6b6faca9d9a0f1ef7bffe5ca9c11e6b
ravidora2004 <42553644+ravidora2004@users.noreply.github.com>
2025-06-07 21:33:20 -0500

    Update search.component.ts

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,3 +1,5 @@
-<option value="@item.Key" @(Model.Country != null && Model.Country.Contains(item.Key) ? "selected" : null)>
-    @item.Value
-</option>
+@foreach (var item in Model.CountryList)
+{
+    var selected = Model.Country != null && Model.Country.Contains(item.Key) ? "selected" : "";
+    <option value="@item.Key" @selected>@item.Value</option>
+}
