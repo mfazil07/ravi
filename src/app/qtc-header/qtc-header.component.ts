16fef4caa4d0942e6d17f1be517f8cbe70ce29f2
ravidora2004 <42553644+ravidora2004@users.noreply.github.com>
2025-06-09 16:25:26 -0500

    Update qtc-header.component.ts

--- src/app/qtc-header/qtc-header.component.ts
+++ src/app/qtc-header/qtc-header.component.ts
@@ -1,3 +1,11 @@
 <input type="hidden" id="weatherAlertCountryId" value="@idDDCountry" />
 <input type="hidden" id="weatherAlertStateId" value="@idDDState" />
 <input type="hidden" id="weatherAlertEventId" value="@idDDWeatherEvent" />
+
+
+var countryId = $('#weatherAlertCountryId').val();
+var stateId = $('#weatherAlertStateId').val();
+var eventId = $('#weatherAlertEventId').val();
+
+// Use these IDs instead of hardcoded ones
+$('#' + countryId)...
